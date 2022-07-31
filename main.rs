7a95e88fc168dae02f2683ea303469ba2854607f
Dappstr <87837549+Dappstr@users.noreply.github.com>
2022-07-31 16:53:15 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,19 +1,33 @@
-use std::io;
+#![allow(unused)]
+
+use std::io::{*, self};
+use std::fs::File;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main()
 {
-    println!("Enter a number: ");
+    let rand_num = rand::thread_rng().gen_range(1..11);
+    let mut guesses: i32 = 0;
+    let mut guess: String = String::new();
 
-    let mut num = String::new();
-
-    io::stdin().read_line(&mut num).expect("Bad input");
-
-    let x: i32 = num.trim().parse::<i32>().unwrap();
-    let mut num: i32 = 1;
     loop {
-        if(num % 2 == 0) { println!("{}", num);}
-        num += 1;
+        guesses+=1;
+        assert!(guesses < 6);
 
-        if num == x {break;}
+        println!("Enter guess between 1 and 10");
+        guess.clear();
+        io::stdin().read_line(&mut guess).expect("Bad input");
+
+        let mut guess: i32 = guess.trim_end().parse::<i32>().unwrap();
+
+        if(guess == rand_num) { 
+            println!("You guessed correctly");
+            break;
+        }
+        else {
+            print!("Incorrect! Try again.");
+            io::stdout().flush();
+        }
     }
 }
