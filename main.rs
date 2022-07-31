fcde94c9f01604e5ddd6ec797e1f6a8ae0874c07
Dappstr <87837549+Dappstr@users.noreply.github.com>
2022-07-31 16:59:08 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,6 +5,22 @@ use std::fs::File;
 use std::cmp::Ordering;
 use rand::Rng;
 
+/*
+println!("Enter a number: ");
+
+    let mut num = String::new();
+
+    io::stdin().read_line(&mut num).expect("Bad input");
+
+    let x: i32 = num.trim().parse::<i32>().unwrap();
+    let mut num: i32 = 1;
+    loop {
+        if(num % 2 == 0) { println!("{}", num);}
+        num += 1;
+
+        if num == x {break;}
+    }*/
+
 fn main()
 {
     let rand_num = rand::thread_rng().gen_range(1..11);
